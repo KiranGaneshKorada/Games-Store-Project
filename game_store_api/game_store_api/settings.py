049adbe9ea18fe7b748c1bb49fb335e1deb08d8f
Kiran Ganesh Korada <kiranganeshkorada@gmail.com>
2023-09-01 21:08:09 +0530

    configured settings.py

--- game_store_api/game_store_api/settings.py
+++ game_store_api/game_store_api/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'games_app',
 ]
 
 MIDDLEWARE = [
@@ -74,9 +76,11 @@ WSGI_APPLICATION = 'game_store_api.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+     'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'gamesdb',
+        'USER':'root',
+        'PASSWORD':'likkiran8Rslk4'
     }
 }
 
