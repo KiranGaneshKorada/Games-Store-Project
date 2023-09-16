a347f24fd4795fec91a57092d62d22a8808e95de
Kiran Ganesh Korada <kiranganeshkorada@gmail.com>
2023-09-17 00:29:37 +0530

    image issue

--- game-store/src/services/image-resize.tsx
+++ game-store/src/services/image-resize.tsx
@@ -1,6 +1,7 @@
-
+import imageNotFound from '../assets/image-previews/imageNotFound.png'
 
 const imageResize=(url:string)=>{
+    if (!url) return imageNotFound
     const bit=url.indexOf("media/")
     url=url.slice(0,bit+6)+"crop/600/400/"+url.slice(bit+6)
     return url
