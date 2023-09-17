8937219cf8d2e38c234e19b3cc3c1ef34b0a66f8
Kiran Ganesh Korada <kiranganeshkorada@gmail.com>
2023-09-17 18:21:46 +0530

    deleted django api

--- game_store_api/games_app/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Game(models.Model):
-    name=models.CharField(max_length=30)
-    description=models.TextField()
-    released=models.DateField()
-    background_image=models.ImageField(upload_to="games_bg_images")
-    website=models.CharField()
