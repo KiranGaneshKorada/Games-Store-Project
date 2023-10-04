b68c1b624e9b19c27b92dc34ad57a2b0e8fe08bb
Kiran Ganesh Korada <kiranganeshkorada@gmail.com>
2023-10-05 00:22:20 +0530

    done with error page

--- game-store/src/routes.tsx
+++ game-store/src/routes.tsx
@@ -2,16 +2,18 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import MainComponent from "./components/MainComponent";
 import GameDetails from "./components/GameDetails";
+import ErrorPage from "./components/ErrorPageComponent";
 
 const router = createBrowserRouter([
   {
     path: "/games",
     element: <App />,
     children: [
-      { path: "", element: <MainComponent /> },
-      { path: ":id", element: <GameDetails /> },
+      { path: "", element: <MainComponent />},
+      { path: ":id", element: <GameDetails />}
     ],
   },
+  {path:'*',element:<ErrorPage/>}
 ]);
 
 export default router;
